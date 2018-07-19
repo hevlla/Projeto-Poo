5a60306d1a49e7304f59923b4ec7a1fed069f601
claudio_alberto <38073359+claudio-alberto@users.noreply.github.com>
2018-07-19 01:54:05 -0300

    Update DetectaCaracteresNaPlaca.py

--- DetectaCaracteresNaPlaca.py
+++ DetectaCaracteresNaPlaca.py
@@ -10,29 +10,28 @@ class DetectaCaracteres:
         imagem_placa = imagem
         cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
         ret, imagem = cv2.threshold(cinza, self.LIMIAR, 255, cv2.THRESH_BINARY)
+        cv2.imshow("Binaria-Caracteres", imagem)
+        cv2.waitKey(0)
         imagem_desfocada = cv2.GaussianBlur(imagem, self.TAMANHO_DO_FILTRO_GAUSSIANO, 0)
         return imagem_desfocada, imagem_placa
 
     def detectaCaracteres(self, imagem_desfocada, imagem_placa_original):
 
         _, contornos, hier = cv2.findContours(imagem_desfocada, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        regioes_retangulares = []
+        possiveis_caracteres = []
 
         for c in contornos:
             perimetro = cv2.arcLength(c, True)
-            if perimetro > 250:
+            if perimetro > 200:
                 aprox = cv2.approxPolyDP(c, 0.02 * perimetro, True)
                 (x, y, alt, lar) = cv2.boundingRect(c)
                 cv2.rectangle(imagem_placa_original, (x, y), (x + alt, y + lar), (0, 0, 255), 2)
                 possivel_placa = imagem_placa_original[y:y + lar, x:x + alt]
-                regioes_retangulares.append(possivel_placa)
+                possiveis_caracteres.append(possivel_placa)
 
-        for i in regioes_retangulares:
-            cv2.imshow("foto", i)
-            cv2.waitKey(0)
 
         cv2.imshow('Possiveis Caracteres', imagem_placa_original)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        return regioes_retangulares
+        return possiveis_caracteres
 
