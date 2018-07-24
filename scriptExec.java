c0296d80661bbef8d7569dd4bf8f524e2b9c74f7
claudio_alberto <38073359+claudio-alberto@users.noreply.github.com>
2018-07-24 13:58:06 -0300

    Comunicando os dois códigos

--- scriptExec.java
+++ scriptExec.java
@@ -1,25 +1,31 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class scriptExec {
 
-    public static void main(String args[]) { 
+    public static void main(String args[]) throws IOException { 
         String path = null;
         System.out.print("\n#Executando script#"); 
-        System.out.print("\n##Inciando processo"); 
-
-        try {
+        System.out.print("\n##Inciando processo\n"); 
+        String s = null;
         
-            String[] commands = {"/bin/bash","-c",
-                                "python3", "home/claudio/PycharmProjects/Projetos/main.py", "--image", 
-                                "home/claudio/PycharmProjects/Projetos/placa3.jpg"};
+        try {
+            
+            
+            Process p = Runtime.getRuntime().exec("python3 //home//claudio//PycharmProjects//Projetos//main.py --image  //home//claudio//PycharmProjects//Projetos//placa3.jpg");
 
-            //Process proc = new ProcessBuilder(commands).start();
-            Runtime.getRuntime().exec("/bin/bash -c python3 home/claudio/PycharmProjects/Projetos/main.py --image "
-                    + "home/claudio/PycharmProjects/Projetos/placa3.jpg");
-
+            BufferedReader stdInput = new BufferedReader (new InputStreamReader (p.getInputStream ()));
+            System.out.println ("Aqui está a saída padrão do comando: \n");
+            
+            while ((s = stdInput.readLine())!= null){ 
+                System.out.println(s);
+            }
             System.out.print("\n##Processo Finalizado.");
-        } 
+    }
 
         catch (IOException e) { 
             e.printStackTrace(); 
